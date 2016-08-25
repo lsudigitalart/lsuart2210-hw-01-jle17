36894fc3c7cd92989b9c1fc4ef56e4e88a4068f6
Jessica Le <Jessica@80-e6-50-9-8d-6e.wlan.lsu.edu>
2016-08-25 11:31:35 -0500

    done

---
+++ sketch.js
@@ -0,0 +1,57 @@
+function setup() {
+  createCanvas(1000, 1000);
+
+  //flower 1
+  ellipse(100, 100, 50);
+    //petals
+    ellipse(100, 60, 30);
+    ellipse(60, 100, 30);
+    ellipse(140, 100, 30);
+    ellipse(100, 140, 30);
+    ellipse(70, 70, 30);
+    ellipse(130, 70, 30);
+    ellipse(70, 130, 30);
+
+  //flower 2
+  ellipse(200, 300, 50);
+    //petals
+    ellipse(200, 260, 30);
+    ellipse(160, 300, 30);
+    ellipse(240, 300, 30);
+    ellipse(200, 340, 30);
+    ellipse(170, 270, 30);
+    ellipse(230, 270, 30);
+    ellipse(170, 330, 30);
+
+  //flower 3
+  ellipse(350, 200, 50);
+    //petals
+    ellipse(350, 160, 30);
+    ellipse(310, 200, 30);
+    ellipse(390, 200, 30);
+    ellipse(350, 240, 30);
+    ellipse(320, 170, 30);
+    ellipse(380, 170, 30);
+
+  //flower 4
+  ellipse(500, 350, 50);
+    //petals
+    ellipse(500, 310, 30);
+    ellipse(460, 350, 30);
+    ellipse(540, 350, 30);
+    ellipse(500, 390, 30);
+    ellipse(470, 320, 30);
+    ellipse(530, 320, 30);
+
+  //flower 5
+  ellipse(600, 150, 50);
+    //petals
+    ellipse(600, 110, 30);
+    ellipse(560, 150, 30);
+    ellipse(640, 150, 30);
+    ellipse(600, 190, 30);
+    ellipse(570, 120, 30);
+    ellipse(630, 120, 30);
+
+
+}
